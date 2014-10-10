3dcac7cf9fbd89ceac93c8079067da9b5fd62fdc
alkyl1978 <alkyl1978@yandex.ru>
2014-10-10 11:51:16 +0400

    add files

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #include "inc/hw_sysctl.h"
 #include "inc/hw_types.h"
 #include "inc/hw_timer.h"
-#include "inc/gpio.h"
-#include "inc/sysctl.h"
-#include "inc/interrupt.h"
-#include "inc/timer.h"
-#include "inc/rom.h"
-#include "inc/pin_map.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+#include "driverlib/rom.h"
+#include "driverlib/pin_map.h"
 #include "include/servo.h"
 #include "include/led.h"
 #include "include/dvig.h"
@@ -71,10 +71,7 @@ int main()
   lcd_init();
   Dvig_init();
   foto_init();
-  UARTStdioInit(0);
   ROM_IntMasterEnable();
-
-  UARTprintf("Welcome to the Stellaris LM4F120 LaunchPad!\n");
   lcd_goto(0,0);
   lcd_puts("---------------"); 
   lcd_goto(1,0);
