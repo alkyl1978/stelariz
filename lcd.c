7497a61178eec38f487b0228ce60f96b47bcf39c
alkyl1978 <alkyl1978@yandex.ru>
2014-10-16 15:50:14 +0400

    fix

--- lcd.c
+++ lcd.c
@@ -19,7 +19,7 @@
 void lcd_temizle(void)
 {  
   lcd_komut(0x01);
-  ROM_SysCtlDelay(ROM_SysCtlClockGet()/100); // 10ms
+  ROM_SysCtlDelay(ROM_SysCtlClockGet()/300); // 10ms
 }
 
 
@@ -43,6 +43,7 @@ void lcd_init(void)
 void lcd_komut(unsigned char c)
 {
   LCD_RS(0);  
+  ROM_SysCtlDelay(ROM_SysCtlClockGet()/1500); // 2ms
   ROM_GPIOPinWrite(GPIO_PORTA_BASE,0xF0,0);
   ROM_GPIOPinWrite(GPIO_PORTA_BASE,0xF0,(c & 0xF0));
   LCD_EN(1);
@@ -56,7 +57,8 @@ void lcd_komut(unsigned char c)
 
 void lcd_putch(char c)
 {
-  LCD_RS(1);       
+  LCD_RS(1);  
+  ROM_SysCtlDelay(ROM_SysCtlClockGet()/1500); // 2ms
   ROM_GPIOPinWrite(GPIO_PORTA_BASE,0xF0,0);
   ROM_GPIOPinWrite(GPIO_PORTA_BASE,0xF0,(c & 0xF0));
   EN_ACKAPA();
