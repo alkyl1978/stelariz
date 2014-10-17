534878b48c9743934284e40b9a01a41819bd203f
alkyl1978 <alkyl1978@yandex.ru>
2014-10-17 13:35:41 +0400

    uart test

--- main.c
+++ main.c
@@ -62,6 +62,20 @@ void SysTickIntHandler(void)
 {
 
 }
+
+void UARTSend(const unsigned char *pucBuffer, unsigned long ulCount)
+{
+    //
+    // Loop while there are more characters to send.
+    //
+    while(ulCount--)
+    {
+        //
+        // Write the next character to the UART.
+        //
+        ROM_UARTCharPutNonBlocking(UART0_BASE, *pucBuffer++);
+    }
+}
 //******************************************************************************************************
 //
 //
@@ -101,6 +115,7 @@ int main()
   ROM_IntMasterEnable();
    while(1)
   {
+    UARTSend("<QZ>",4);
     eMBPoll();
   }
   return 0;
